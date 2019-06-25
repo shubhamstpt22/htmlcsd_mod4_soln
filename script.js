5d53a0486619c41fe8c996856653ecf49d97c6fb
shubhamstpt22 <46398550+shubhamstpt22@users.noreply.github.com>
2019-06-25 13:05:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ Good Bye Jim
 */
 
 (function() {
-    var names = ["Shubham", "Aish", "Smita", "Jui", "Pooja", "Shradha", "Sumeet", "Chaitali", "Ram", "Jeevan"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
